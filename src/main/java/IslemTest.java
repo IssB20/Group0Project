71719120799a39ba34f1848976a617b9fd65b6a3
IssB20 <islem_ouiddir@yahoo.fr>
2020-11-03 18:47:06 -0500

    change from islem master

--- src/main/java/IslemTest.java
+++ src/main/java/IslemTest.java
@@ -2,6 +2,7 @@ public class IslemTest {
     public static void main(String[] args) {
 
         System.out.println("working code");
+        System.out.println("Change from islem master");
     }
 
 }
